6dc4f858a427b9e960c5426589e8a653f512c004
Rihan.z <derihan>
2020-11-13 17:55:15 +0700

    Add project files.

---
+++ SimpleMVVM/Model/StudentModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SimpleMVVM.Model
+{
+   
+    public class StudentModel{ }
+
+    public class Student : INotifyPropertyChanged
+    {
+        
+
+        private string firstName;
+        private string lastName;
+
+        public string Firstname
+        {
+            get {
+                return firstName;
+            }
+            set {
+                if(firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertyChanged("FirstName");
+                    RaisePropertyChanged("Fullname");
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get { return lastName; }
+            set
+            {
+                if(lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertyChanged("Lastname");
+                    RaisePropertyChanged("Fullname");
+                }
+            }
+        }
+
+        public string Fullname
+        {
+            get { return firstName + " " + lastName; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+    }
+
+
+}
---
+++ SimpleMVVM/ViewModel/StudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleMVVM.Model;
+using System.Collections.ObjectModel;
+
+namespace SimpleMVVM.ViewModel
+{
+    class StudentViewModel
+    {
+        public ObservableCollection<Student> Students 
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>
+            {
+                new Student{ Firstname = "Mark",Lastname="Allain" },
+                new Student{ Firstname = "Allen", Lastname = "Brown" },
+                new Student{ Firstname = "Linda",Lastname="Hamerski" }
+            };
+
+            Students = students;
+        }
+    }
+}
